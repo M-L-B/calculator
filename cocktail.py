417c2706cc35ec3eb9afe8812cf89be70238d323
morgan <morganhsu0618@gmail.com>
2022-09-04 08:41:42 -0400

    cooktail

--- cocktail.py
+++ cocktail.py
@@ -1,12 +1,21 @@
 def cocktail(num):
     leng=len(num)
-    while leng-1>0:
+    while leng/2-1>0:
       i=1
-      while i<=leng-1 :
+      j=0
+      while j<leng-2*i+1 :
         if(num[i]<num[i-1]):
             num[i],num[i-1]=num[i-1],num[i]
-        i+=1
-      leng-=1
+        print(num)
+        j+=1
+      print('--')
+      j=0
+      while j<leng-2*i:
+        if(num[int(leng-i-1)]<num[leng-i]):
+            num[int(leng-i-1)],num[leng-i]=num[leng-i],num[int(leng-i-1)]
+        print(num)
+        j+=1
+      i+=1
     return num
 list=input('輸入一個list')
 num=list.split()
